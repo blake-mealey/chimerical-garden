62b0759badb3836b123826e1b675448f1af01281
Blake Mealey <blakemealey@gmail.com>
2020-06-03 21:41:19 -0600

    added posts page

---
+++ src/pages/posts.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const PostsPage = () => (
+  <Layout title="/blake/posts">
+    <SEO title="Posts" />
+    <p>TODO: List of posts</p>
+  </Layout>
+);
+
+export default PostsPage;
