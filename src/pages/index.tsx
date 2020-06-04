b42f4769e1cfbdab4d26938fd1b09bbee1ac4284
Blake Mealey <blakemealey@gmail.com>
2020-06-04 08:57:41 -0600

    Rename homepage to tilda

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 const IndexPage = () => (
-  <Layout title="/blake">
+  <Layout title="~">
     <SEO title="Home" />
     <p>TODO: About me</p>
   </Layout>
