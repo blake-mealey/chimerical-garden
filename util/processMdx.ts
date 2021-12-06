eb2196b3aeabd275073a03ad5ff789598cc2427b
Blake Mealey <blakemealey@gmail.com>
2021-12-05 22:06:30 -0600

    added code theming

--- util/processMdx.ts
+++ util/processMdx.ts
@@ -1,14 +1,18 @@
 import { serialize } from 'next-mdx-remote/serialize';
 import imageSize from 'rehype-img-size';
-import { dirname } from 'path';
+import prism from 'remark-prism';
 
 async function processMdx(source: string, file: string) {
-  console.log('FILE', file);
-
   return await serialize(source, {
     mdxOptions: {
       filepath: file,
       rehypePlugins: [[imageSize, { dir: 'public' }]],
+      remarkPlugins: [
+        [
+          prism,
+          { theme: 'Night Owl', extensions: ['night-owl', 'vscode-toml'] },
+        ],
+      ],
     },
   });
 }
