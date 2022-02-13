e1783d95892a69c6ff9083bf6f0112826cab07db
Blake Mealey <blake.mealey@vercel.com>
2022-02-13 14:31:30 -0600

    fix splitbee script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Main, NextScript, Head } from 'next/document';
-import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -9,7 +8,7 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;500;600;700&display=swap"
         />
-        <Script src="https://cdn.splitbee.io/sb.js" async />
+        <script src="https://cdn.splitbee.io/sb.js" async></script>
       </Head>
       <body>
         <Main />
